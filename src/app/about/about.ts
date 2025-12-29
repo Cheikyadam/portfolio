1c43fdbc520edf43c17caf7e5d4d436283a828ef
cheikyadam <132616322+cheikyadam@users.noreply.github.com>
2025-12-29 21:25:31 +0100

    chore: code cleanup

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { ContainerComponent, ColComponent, RowComponent, CardBodyComponent, CardComponent, CardHeaderComponent } from "@coreui/angular";
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { ionLogoLinkedin , ionMail, ionLogoGithub, ionLogoDiscord, ionCodeSlash, ionPerson } from '@ng-icons/ionicons';
+import { ionLogoLinkedin , ionMail, ionLogoGithub, ionLogoDiscord, ionCodeSlash, ionPerson, ionCall } from '@ng-icons/ionicons';
 
 @Component({
   selector: 'app-about',
   imports: [ContainerComponent, ColComponent, RowComponent, NgIcon, CardBodyComponent, CardComponent, CardHeaderComponent],
   templateUrl: './about.html',
   styleUrl: './about.scss',
-  viewProviders: [provideIcons({ ionLogoLinkedin, ionMail, ionLogoGithub , ionLogoDiscord, ionCodeSlash, ionPerson })]
+  viewProviders: [provideIcons({ ionLogoLinkedin, ionMail, ionLogoGithub , ionLogoDiscord, ionCodeSlash, ionPerson, ionCall })]
 })
 export class About {
 
--- src/app/skills/skills.ts
+++ src/app/skills/skills.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { ContainerComponent, RowComponent, ColComponent, BadgeComponent, CardComponent, CardBodyComponent } from "@coreui/angular";
 import { NgIcon, provideIcons } from "@ng-icons/core";
-import { ionCloudUpload, ionCodeSlash, ionConstruct, ionLogoHtml5, ionServer } from '@ng-icons/ionicons';
+import { ionCloudUpload, ionCodeSlash, ionConstruct, ionLogoHtml5, ionPersonCircle, ionServer } from '@ng-icons/ionicons';
 
 @Component({
   selector: 'app-skills',
   imports: [ContainerComponent, RowComponent, ColComponent, BadgeComponent, NgIcon, CardComponent, CardBodyComponent],
   templateUrl: './skills.html',
   styleUrl: './skills.scss',
-  viewProviders: [provideIcons({ ionCodeSlash, ionLogoHtml5, ionServer, ionCloudUpload, ionConstruct  })]
+  viewProviders: [provideIcons({ ionCodeSlash, ionLogoHtml5, ionServer, ionCloudUpload, ionConstruct, ionPersonCircle  })]
 
 })
 export class Skills {
